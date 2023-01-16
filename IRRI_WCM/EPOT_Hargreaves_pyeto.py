1c4d7f0f6c5e52f6f3181f6cca3f9410f1bf7317
marti <martina01.natali@edu.unife.it>
2023-01-16 15:49:21 +0100

    Worked on v5: adjusted timing consistency of s0, field data

--- IRRI_WCM/EPOT_Hargreaves_pyeto.py
+++ IRRI_WCM/EPOT_Hargreaves_pyeto.py
@@ -1,12 +1,28 @@
 # Ref. https://pyeto.readthedocs.io/en/latest/hargreaves.html
 
-import datetime, pyeto
+# ET0 calculation
 
-def hargre(lat_rad, date:datetime.date, temp_min, temp_max, temp_mean)
-    lat = pyeto.deg2rad(lat_rad)  # Convert latitude in degrees to radians
-    day_of_year = date.timetuple().tm_yday
-    sol_dec = pyeto.sol_dec(day_of_year)            # Solar declination
-    sha = pyeto.sunset_hour_angle(lat, sol_dec)
-    ird = pyeto.inv_rel_dist_earth_sun(day_of_year)
-    et_rad = pyeto.et_rad(lat, sol_dec, sha, ird)   # Extraterrestrial radiation
-    hargreaves(temp_min, temp_max, temp_mean, et_rad)
+import datetime
+import sys
+
+sys.path.append('../')
+from modules.pyeto.pyeto import *
+
+def hargre(lat_deg, dates, temp_min, temp_max, temp_mean):
+    """Hargreaves-Samani model for ET0 estimation from temperature input.
+    
+    Params
+    ------
+    - lat_deg: float
+    - dates: timestamp
+    - temp_*: float
+    
+    
+    """
+    lat = deg2rad(lat_deg)  # Convert latitude in degrees to radians
+    day_of_year = dates.dayofyear
+    sol_decli = sol_dec(day_of_year) # Solar declination
+    sha = sunset_hour_angle(lat, sol_decli)
+    ird = inv_rel_dist_earth_sun(day_of_year)
+    et_radia = et_rad(lat, sol_decli, sha, ird) # Extraterrestrial radiation
+    return hargreaves(temp_min, temp_max, temp_mean, et_radia)
