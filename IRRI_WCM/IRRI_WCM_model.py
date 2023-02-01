ae968648a390fdb96549ba2e7761079435b04af9
martina01natali <martina01.natali@edu.unife.it>
2023-02-01 21:19:17 +0100

    run 20 runs in db

--- IRRI_WCM/IRRI_WCM_model.py
+++ IRRI_WCM/IRRI_WCM_model.py
@@ -58,27 +58,53 @@ def IRR_WCM(PAR, inputs, user_in):
     irri, units = user_in
     
     # Unpack inputs
-    A, B, C, D, W_max, Kc = PAR
+    A, B, C, D, W_max, WW_fc, WW_w, rho_st, Kc0 = PAR
     d, d_sat, P, IRR_obs, EPOT, W_d, W_h, veg, theta, VV = inputs
     
     # Fixed parameters
+    # global WW_fc  # = 0.32 # 0.32
+    # global WW_w   # = 0.08 # 0.08
+    # global rho_st # = 0.4 # /24 # 0.4
+    # global Kc0    # = 1 # 0.05 # 1
     W_0 = W_d[0]
-    WW_fc = 0.32
-    WW_w = 0.08
-    rho_st = 0.4    
     
-    angle = theta*np.pi/180. # angle of incidence
-    W_fc = WW_fc*W_max # field capacity [mm]
-    W_w = WW_w*W_max # wilting point [mm]
-    Ks = [0]*len(d) # daily, water stress coefficient
-    rho = [0]*len(d) # daily, depletion fraction
-    PS = [0]*len(d) # daily, deep percolation
-    W = [0]*len(d) # daily, water content [mm]
-    W[0] = W_0*W_max # initial value of sm [mm]
+    angle  = theta*np.pi/180. # angle of incidence
+    W_fc   = WW_fc*W_max # field capacity [mm]
+    W_w    = WW_w*W_max # wilting point [mm]
+    Ks     = [0]*len(d) # daily, water stress coefficient
+    rho    = [0]*len(d) # daily, depletion fraction
+    PS     = [0]*len(d) # daily, deep percolation
+    W      = [0]*len(d) # daily, water content [mm]
+    W[0]   = W_0*W_max # initial value of sm [mm]
     if irri==True: IRR = [0]*len(d) # daily, water content
     else: IRR = IRR_obs
     
     for t in [i+1 for i in range(len(d)-1)]:
+        
+        DOY=d[t].dayofyear
+        
+        # Build Kc curve
+        # Ref. FAO56, Tables 11, 12
+        START  = 134 # 14 May
+        Kc_ini = 0.6*Kc0
+        Kc_mid = 1.15*Kc0
+        Kc_late= 0.7*Kc0
+
+        INI =[x for x in range(START, START+30)] # initial phenological phase
+        DEV =[x for x in range(START+30, START+30+40)] # development phase
+        m1  =(Kc_mid-Kc_ini)/40 # daily Kc growth in DEV phase 
+        MID =[x for x in range(START+30+40, START+30+40+45)] # mid phase
+        LATE=[x for x in range(START+30+40+45, START+30+40+45+30)] # late stage
+        m2  =(Kc_late-Kc_mid)/30 # daily Kc growth in senescence
+        
+        if DOY in INI: Kc=Kc_ini
+        elif DOY in DEV: Kc=Kc_ini+m1*(DOY-DEV[0])
+        elif DOY in MID: Kc=Kc_mid
+        elif DOY in LATE: Kc=Kc_mid+m2*(DOY-LATE[0])
+        elif DOY>LATE[-1]: Kc=Kc_late
+        else: Kc=Kc_ini
+        
+        # Build Ks curve
         rho[t]=rho_st+0.04*(5-Kc*EPOT[t])
         if W[t-1]>=(1-rho[t])*W_fc:
             Ks[t]=1
@@ -86,13 +112,11 @@ def IRR_WCM(PAR, inputs, user_in):
             Ks[t]=float(W[t-1]-W_w)/((1-rho[t])*(W_fc-W_w))
         else: Ks[t]=0
         
-        DOY=d[t].dayofyear
-        
         # Irrigation estimate (for summer season only)
         # Irrigation is estimated as the amount of water needed from the day
         # before to take water content up to field capacity
         if irri==True:
-            if np.logical_and(DOY>134,DOY<235): # summer season
+            if np.logical_and(DOY>START,DOY<START+100): # summer season
                 if W[t-1]<=(1-rho[t])*W_fc: IRR[t]=W_fc-W[t-1]
         
         # Water balance [mm]
@@ -104,30 +128,166 @@ def IRR_WCM(PAR, inputs, user_in):
             W[t]=W_fc
             
     WW=np.array(W)/W_max
-    WWsat = pd.DataFrame(timeseries(d,WW)).set_index(0).loc[d_sat][1].values
+    # WWsat = pd.DataFrame(timeseries(d,WW)).set_index(0).loc[d_sat][1].values # old, slower method
+    WWsat = np.array([ x[1] for x in timeseries(d,WW) if x[0] in d_sat ])
     
+    # Water Cloud Model
     
-    sig0s_dB = C+D*WWsat # sigma0_soil [dB]
-    T2 = np.exp((-2*B*veg)/np.cos(angle)) # attenuation
-    
-    if units=='lin':
-        sig0s = db_lin(sig0s_dB) # sigma0_soil [lin]
-        sig0v = A*veg*np.cos(angle)*(1-T2) # sigma0_veg [lin]
-        sig0_lin = T2*sig0s+sig0v # sigma0_tot [lin]
-        sig0=lin_db(sig0_lin) # sigma0_tot [dB]
-    elif units=='db':
-        sig0v = A*veg*np.cos(angle)*(1-T2) # sigma0_veg [db]
-        sig0 = T2*sig0s+sig0v # sigma0_tot [db]
-    else: raise NameError('Please choose one of the options: lin/db')
+    # sig0s_dB = C+D*WWsat # sigma0_soil [dB]
+    # T2 = np.exp((-2*B*veg)/np.cos(angle)) # attenuation
+    # sig0v = A*veg*np.cos(angle)*(1-T2) # sigma0_veg [units]
+    # 
+    # if units=='lin':
+    #     sig0s = db_lin(sig0s_dB) # sigma0_soil [lin]
+    #     sig0_lin = T2*sig0s+sig0v # sigma0_tot [lin]
+    #     sig0=lin_db(sig0_lin) # sigma0_tot [dB]
+    # elif units=='db':
+    #     sig0 = T2*sig0s_dB+sig0v # sigma0_tot [db]
+    # else: raise NameError('Please choose one of the options: lin/db')
 
-    
-    # T2 = np.exp((-2*B*veg)/np.cos(angle)) # two-way attenuation from the vegetation layer
-    # sig0s = db_lin(C+D*WWsat) # define bare soil backscatter [fit in dB, then in lin]
-    # sig0v = A*veg*np.cos(angle)*(1-T2) # define backscatter from the vegetation [fit in lin]
-    # sig0_lin = T2*sig0s+sig0v
-    # sig0=lin_db(sig0_lin) # from linear scale to dB
+    # single-line version
+    if units=='lin':
+        sig0 = lin_db((np.exp((-2*B*veg)/np.cos(angle)))*(db_lin(C+D*WWsat))\
+                      +A*veg*np.cos(angle)*(1-(np.exp((-2*B*veg)/np.cos(angle))))) # sigma0_tot [dB]
+    elif units=='db':
+        sig0 = (np.exp((-2*B*veg)/np.cos(angle)))*(C+D*WWsat)\
+        +(A*veg*np.cos(angle)*(1-(np.exp((-2*B*veg)/np.cos(angle))))) # sigma0_tot [db]
+    else: raise NameError('Please choose one of the options: lin/db')
         
-    OUT=he.evaluator(he.kge, sig0, VV) # OUT is kge, r, alpha, beta
+    OUT=he.evaluator(he.kge, sig0, VV)
+    KGE=OUT[0,:];
+
+    return [WW,IRR,sig0,KGE]
+
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+
+def IRR_WCM_allpar(PAR, inputs, user_in):
+    """Irrigation model and WCM integration.
+    
+    Based on minimization of KGE between observed and simulated
+    $\sigma^0$ values via PSO (pyswarm) optimization.
+    The soil water balance model (IRR) produces an estimate of the soil water
+    content WW [%] that is used to simulate $\sigma^0$ by a water cloud
+    model (WCM).
+    
+    Inputs
+    ----------
+    - PAR: initial guess values for parameters to calibrate
+        PAR = [A, B, C, D, W_0, W_max, S_fc, S_w, rho_st, Kc]
+    - inputs: input quantities for calibration,
+        [d, d_sat, P, IRR_obs, EPOT, WWobs, LAI, t_deg, obs]
+    - user_in: user-defined options
+        irri = user_in: if user_in=True, irrigation is estimated
+        and not taken as an input, else the input observed irrigation
+        is used in the soil water balance
+    
+    Return
+    -------
+    KGE from hydroeval between sigma0 observed and simulated.
+    
+    """
+
+    # User input
+    irri, units = user_in
+    
+    # Unpack inputs
+    A, B, C, D, W_max, WW_fc, WW_w, rho_st, Kc0 = PAR
+    d, d_sat, P, IRR_obs, EPOT, W_d, W_h, veg, theta, VV = inputs
+    
+    # Fixed parameters
+    W_0    = W_d[0]
+    # WW_fc  = 0.35 # 0.32
+    # WW_w   = 0.12 # 0.08
+    # rho_st = 0.4
+    # Kc0     = 1
+    
+    angle  = theta*np.pi/180. # angle of incidence
+    W_fc   = WW_fc*W_max # field capacity [mm]
+    W_w    = WW_w*W_max # wilting point [mm]
+    Ks     = [0]*len(d) # daily, water stress coefficient
+    rho    = [0]*len(d) # daily, depletion fraction
+    PS     = [0]*len(d) # daily, deep percolation
+    W      = [0]*len(d) # daily, water content [mm]
+    W[0]   = W_0*W_max # initial value of sm [mm]
+    if irri==True: IRR = [0]*len(d) # daily, water content
+    else: IRR = IRR_obs
+    
+    for t in [i+1 for i in range(len(d)-1)]:
+        
+        DOY=d[t].dayofyear
+        
+        # Build Kc curve
+        # Ref. FAO56, Tables 11, 12
+        START  = 134
+        Kc_ini = 0.6*Kc0
+        Kc_mid = 1.15*Kc0
+        Kc_late= 0.7*Kc0
+
+        INI =[x for x in range(START, START+30)] # initial phenological phase
+        DEV =[x for x in range(START+30, START+30+40)] # development phase
+        m1  =(Kc_mid-Kc_ini)/40 # daily Kc growth in DEV phase 
+        MID =[x for x in range(START+30+40, START+30+40+45)] # mid phase
+        LATE=[x for x in range(START+30+40+45, START+30+40+45+30)] # late stage
+        m2  =(Kc_late-Kc_mid)/30 # daily Kc growth in senescence
+        
+        if DOY in INI: Kc=Kc_ini
+        elif DOY in DEV: Kc=Kc_ini+m1*(DOY-DEV[0])
+        elif DOY in MID: Kc=Kc_mid
+        elif DOY in LATE: Kc=Kc_mid+m2*(DOY-LATE[0])
+        elif DOY>LATE[-1]: Kc=Kc_late
+        else: Kc=Kc_ini
+        
+        # Build Ks curve
+        rho[t]=rho_st+0.04*(5-Kc*EPOT[t])
+        if W[t-1]>=(1-rho[t])*W_fc:
+            Ks[t]=1
+        elif (W[t-1]>W_w)and(W[t-1]<(1-rho[t])*W_fc):
+            Ks[t]=float(W[t-1]-W_w)/((1-rho[t])*(W_fc-W_w))
+        else: Ks[t]=0
+        
+        # Irrigation estimate (for summer season only)
+        # Irrigation is estimated as the amount of water needed from the day
+        # before to take water content up to field capacity
+        if irri==True:
+            if np.logical_and(DOY>START,DOY<START+100): # summer season
+                if W[t-1]<=(1-rho[t])*W_fc: IRR[t]=W_fc-W[t-1]
+        
+        # Water balance [mm]
+        W[t]=W[t-1]+P[t]+IRR[t]-EPOT[t]*Kc*Ks[t]
+        
+        # Computation of deep percolation (water above field capacity)
+        if W[t]>W_fc:
+            PS[t]=W[t]-W_fc
+            W[t]=W_fc
+            
+    WW=np.array(W)/W_max
+    WWsat = np.array([ x[1] for x in timeseries(d,WW) if x[0] in d_sat ])
+    
+    # Water Cloud Model
+    
+    # sig0s_dB = C+D*WWsat # sigma0_soil [dB]
+    # T2 = np.exp((-2*B*veg)/np.cos(angle)) # attenuation
+    # sig0v = A*veg*np.cos(angle)*(1-T2) # sigma0_veg [units]
+    # 
+    # if units=='lin':
+    #     sig0s = db_lin(sig0s_dB) # sigma0_soil [lin]
+    #     sig0_lin = T2*sig0s+sig0v # sigma0_tot [lin]
+    #     sig0=lin_db(sig0_lin) # sigma0_tot [dB]
+    # elif units=='db':
+    #     sig0 = T2*sig0s_dB+sig0v # sigma0_tot [db]
+    # else: raise NameError('Please choose one of the options: lin/db')
+
+    # single-line version
+    if units=='lin':
+        sig0 = lin_db((np.exp((-2*B*veg)/np.cos(angle)))*(db_lin(C+D*WWsat))\
+                      +A*veg*np.cos(angle)*(1-(np.exp((-2*B*veg)/np.cos(angle))))) # sigma0_tot [dB]
+    elif units=='db':
+        sig0 = (np.exp((-2*B*veg)/np.cos(angle)))*(C+D*WWsat)\
+        +(A*veg*np.cos(angle)*(1-(np.exp((-2*B*veg)/np.cos(angle))))) # sigma0_tot [db]
+    else: raise NameError('Please choose one of the options: lin/db')
+        
+    OUT=he.evaluator(he.kge, sig0, VV)
     KGE=OUT[0,:];
 
     return [WW,IRR,sig0,KGE]
