8f05b251516e9ebc83594dc293dd65359e603d2a
marti <martina01.natali@edu.unife.it>
2023-03-12 11:53:50 +0100

    analysis plots, timeseries, validation on 2020

--- modules/funcs_analysis.py
+++ modules/funcs_analysis.py
@@ -38,13 +38,12 @@ def gauss(x, sigmag, mu, A):
     return A*pdf
 
 
-def skew_gauss(x, sigmag, mu, alpha, a, c,):
+def skew_gauss(x, sigmag, mu, alpha, a):
     
     normpdf = (1/(sigmag*np.sqrt(2*math.pi)))*np.exp(-(np.power((x-mu),2)/(2*np.power(sigmag,2))))
     normcdf = (0.5*(1+sp.erf((alpha*((x-mu)/sigmag))/(np.sqrt(2)))))
     
-    return 2*a*normpdf*normcdf + c
-
+    return 2*a*normpdf*normcdf
 
 
 #############################################################################
@@ -54,6 +53,7 @@ def skew_gauss(x, sigmag, mu, alpha, a, c,):
 def hist_gauss_fit(data, nbins, hist_kwargs, fitline_kwargs,
                    title, density=False,
                    opt_save=False, dir_name='', opt_name='hist_fit',
+                   opt_fit=True,
                    func='optim', thr_asymm=2, fit_method='trf',
                   ):
     """Histogram with automatic gaussian fit.
@@ -64,9 +64,15 @@ def hist_gauss_fit(data, nbins, hist_kwargs, fitline_kwargs,
         WARNING: skew_gauss not supported yet
         
     """
-
-    mean = np.mean(data); rang=np.ptp(data)
-    x = np.linspace(mean-rang, mean+rang, 1000)
+    
+    from scipy import integrate, interpolate
+    
+    fontdict = {
+        'size':13
+    }
+    
+    mean = np.mean(data); rang=np.ptp(data);
+    x = np.linspace(mean-2*rang, mean+2*rang, 500)
     counts, bins, pads = plt.hist(data, bins=nbins, density=density, **hist_kwargs)
     
     thr_asymm = thr_asymm
@@ -78,36 +84,84 @@ def hist_gauss_fit(data, nbins, hist_kwargs, fitline_kwargs,
     integ = sum(np.diff(bins)*counts)/sum(counts) if density==False else 1
     
     if func_fit==gauss:
-        fit_label='gauss'
+        fit_label='Gaussian'
         kwargs = {
         'bounds': [ 
+            # sigmag, mu, a
             [0,min(data),0],
             [np.std(data)*2,max(data),integ*2] ]
         }
     elif func_fit==skew_gauss:
-        fit_label='skew_norm'
+        fit_label='Skew normal'
         kwargs = {
             'p0' : (np.std(data), np.mean(data),
-                    stats.skew(data), integ, min(data)),
+                    stats.skew(data), integ),
             'bounds' : [
-                [0, min(0,np.mean(data)*2),  min(0,stats.skew(data)*2), min(0,integ*2), min(0,min(data))],
-                [np.std(data)*2, max(0,np.mean(data)*2), max(0,stats.skew(data)*2), max(0,integ*2), max(0,min(data))]
+                # sigmag, mu, alpha, a
+                [0, min(data),  min(0,stats.skew(data)*2), min(0,integ*2),],
+                [np.std(data)*2, max(data), max(0,stats.skew(data)*2), max(0,integ*2),]
             ]
         }
     else: raise ValueError(f'Func {func} is not a valid option.')
+
     
+    if opt_fit==True:
+        options = {
+            # 'loss' : 'cauchy',
+        }
+
+        popt, pcov = curve_fit(func_fit, bins[:-1], counts,
+                               method=fit_method, **kwargs, **options, maxfev=100000)
+        fit = func_fit(x, *popt)
+        plt.plot(x, fit, label=fit_label+' fit', **fitline_kwargs)
+        mode = x[np.argmax(fit)]
+        
+        def func_fit_popt(x): return func_fit(x, *popt)
+        
+        def quantiles(x):
+            integral = integrate.quad(func_fit_popt, -np.inf, np.inf)[0]
+            pdf = lambda x: func_fit(x, *popt)/integral
+            cdf = lambda x: integrate.quad(pdf, -np.inf, x)[0]
+            y = np.array([]); q1 = False; median = False; q3 = False
+            
+            for xi in x:
+                yi = cdf(xi)
+                if (not q1)and(yi>=1/4): q1=xi
+                if (not median)and(yi>=1/2): median=xi
+                if (not q3)and(yi>=3/4): q3=xi
+                y = np.append(y, yi)
+
+            return [q1, median, q3]
+        
+        q1, q2, q3 = quantiles(x)
+
+    else:
+        fit=[]; popt = []; pcov = []
+        data_sorted = np.sort(data)
+        q1 = data_sorted[int(len(data)*1/4)]
+        q2 = data_sorted[int(len(data)*2/4)]
+        q3 = data_sorted[int(len(data)*3/4)]
+        mode = bins[np.argmax(counts)]
+        
+        
+    maxy = np.max([*fit, *counts])
+    plt.vlines(q1, 0, maxy, color='k', linestyle='--', alpha=.7 , label='Q1')
+    plt.vlines(q2, 0, maxy, color='k', linestyle='-', alpha=1 ,  label='Q2')
+    plt.vlines(q3, 0, maxy, color='k', linestyle='--', alpha=.7 , label='Q3')
+
+    ylabel = 'Density' if density else 'Counts';
+    plt.ylabel(ylabel, fontdict=fontdict)
+    plt.xlabel(title, fontdict=fontdict);
+    plt.legend(loc='best', framealpha=.5);
+    mean=np.mean(data); delta=15*(q3-q1); rang=np.ptp(data)
+    plt.xlim(q2-min([delta, rang]), q2+min([delta, rang]));
     
-    popt, pcov = curve_fit(func_fit, bins[:-1], counts, method=fit_method, **kwargs, maxfev=100000)
-    fit = func_fit(x, *popt)
-    plt.plot(x, fit, label=fit_label+' fit', **fitline_kwargs)
-    ylabel = 'Density' if density else 'Counts'; plt.ylabel(ylabel)
-    plt.xlabel(title);
-    plt.legend(loc='best');
+    plt.tick_params(axis='both', which='both', right=False, top = False)
+    plt.tick_params(axis='both', which='both', labelsize=14)
     
     if opt_save: plt.savefig(dir_name+opt_name+'.png', dpi=300)
     
-    return [counts, bins, pads, popt, pcov]
-
+    return [counts, bins, pads, popt, pcov, q1, q2, q3, mode]
 
 #############################################################################
 # Analysis on parameters out of multiple runs
