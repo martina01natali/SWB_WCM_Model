c0cd86dfa7f86ecf87300317ee791d79d599fa01
martina01natali <martina01.natali@edu.unife.it>
2023-06-22 23:39:16 +0200

    Started simulations on WATERSTEM
    - added options to optimizer
    - calibrated on 2018, started other years

--- WATERSTEM/TODO_WATERSTEM.md
+++ WATERSTEM/TODO_WATERSTEM.md
@@ -1,10 +1,8 @@
 # ToDo
 
 - [x] fare PET con temperature (check HARGREAVES aux function)
-- [ ] study Kc static/dynamic with different ranges
-- [ ] [ ] [OPTIMIZER] fix seed number
-- [ ] fix kc, rho_st, w_fc, w_w
-- [ ] get params on first 1 or 2 years then apply on the basis of thesis' conclusions: 
+- [x] [OPTIMIZER] fix seed number
+- [ ] get params on first 1 or 2 years then apply on the basis of thesis' conclusions
 
 # 5-9/06
 Goal: presentation with new results and pixel-by-pixel maps
---
+++ modules/funcs_io.py
@@ -0,0 +1,47 @@
+import os
+import json
+import fnmatch
+
+
+def search_fname(root, pattern):
+
+    file_paths = []
+    file_list = os.listdir(root)
+    for filename in file_list:
+        if fnmatch.fnmatch(filename, pattern):
+            file_paths.append(os.path.join(root, filename))
+    if not file_paths: raise FileNotFoundError("Matching files not found.")
+    else: print("Found matching files:", file_paths )
+
+    control=0
+    chosen_file_path = file_paths
+    opt_read = input('Do you want to read params from file? [[y]/n]')
+    if opt_read=='y' or opt_read=='':
+        if len(file_paths)==1:
+            print(f'You have chosen scapolottina: {file_paths}')
+            chosen_file_path = file_paths[0]
+        else:
+            opt_params_year = input('Which year do you want to use for static parameters A,B,D?')
+            print('Chosen file of year:', opt_params_year)
+            pattern = '*_'+opt_params_year+'_*'
+            for filename in file_paths:
+                if fnmatch.fnmatch(filename, pattern):
+                    if control: raise FileExistsError('Multiple files found: please check for duplicates.')
+                    else:
+                        print(f'You have chosen scapolottina: {filename}')
+                        chosen_file_path = filename
+                        control=1
+
+    return chosen_file_path
+
+
+def read_json(chosen_file_path:str):->dict
+
+    data = 0
+    if os.path.isfile(chosen_file_path):
+        with open(chosen_file_path, "r") as file:
+            data = json.load(file)
+    else:
+        raise FileNotFoundError('File not found.')
+
+    return data
